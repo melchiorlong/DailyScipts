d22536c1e099353f5aefdb225da9ce5f5e691b24
long.tian <Tl65233644>
2022-03-18 17:33:31 +0800

    update

--- WorkingPlanAndOthers/2022/03/t2.py
+++ WorkingPlanAndOthers/2022/03/t2.py
@@ -11,3 +11,9 @@ from datetime import datetime
 factor_list = ['asdas', '']
 factors = list(filter(None, factor_list))
 print(factors)
+
+
+
+s1 = 's3://gvprod/devops/dw/ua_export/google/uac3/2022-03-16/aiolos_gp/data000'
+print("/".join(s1.split('/')[:-1]))
+# 's3://gvprod/devops/dw/ua_export/google/day7_good30/2022-03-15/aiolos_gp'
---
+++ WorkingPlanAndOthers/S3Handle.py
@@ -0,0 +1,28 @@
+from rock.s3.s3_proxy import S3Proxy
+
+s3p = S3Proxy(
+    bucket='gvprod',
+    aws_access_key_id='AKIAWXPN5MB5V4YEBTG5',
+    aws_secret_access_key='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',
+    region_name='us-east-1',
+    endpoint_url='https://s3.amazonaws.com',
+)
+
+s3_prefix = 'data_warehouse/oil_painting/fact_table/behavior/log_time='
+
+key_list_all = s3p.list_key(s3_prefix)
+
+key_list_extend_aws_key = []
+for key in key_list_all:
+    file_name = key.split('/')[-1]
+    if not file_name.startswith('part'):
+        if file_name == '_SUCCESS':
+            continue
+        key_list_extend_aws_key.append(key)
+    try:
+        if len(key_list_extend_aws_key) > 0:
+            for key in key_list_extend_aws_key:
+                s3p.remove_file(key)
+                print(key + " Deleted! ")
+    except Exception as exception:
+        raise exception
