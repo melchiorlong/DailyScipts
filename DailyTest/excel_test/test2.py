493850bdeb31d0112921cfea5893ef4481668168
Long Tian <448704865@qq.com>
2023-10-18 08:32:47 +0800

    upd

--- DailyTest/excel_test/test2.py
+++ DailyTest/excel_test/test2.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 city_name = ''
 data = pd.read_excel('/Users/miaolin/Desktop/样例指标.xlsx')
 index_list = data.columns[1:]
@@ -13,22 +12,18 @@ group_set = data['分组'].toSet()
 res_greater_dict = {}
 res_less_dict = {}
 
-
 for group in group_set:
-    for i in index_list:
-        if group == data.columns[1]:
-            result_list = data[i].tolist()
-            result_list = list(map(float, result_list))
+	fac_list = [index for index, x in enumerate(data.columns[1]) if x == group]
+	for i in index_list:
+		if i in fac_list:
+			result_list = data[i].tolist()
+			result_list = list(map(float, result_list))
 
-            if float(city_result[i]) >= sum(result_list) / len(result_list):
-                result1.append(i)
-                res_greater_dict[group] = result1
-            elif float(city_result[i]) <= sum(result_list) / len(result_list):
-                result2.append(i)
-                res_less_dict[group] = result1
-            else:
-                pass
-
-
-
-
+			if float(city_result[i]) >= sum(result_list) / len(result_list):
+				result1.append(i)
+				res_greater_dict[group] = result1
+			elif float(city_result[i]) <= sum(result_list) / len(result_list):
+				result2.append(i)
+				res_less_dict[group] = result1
+			else:
+				pass
---
+++ DailyTest/excel_test/test3.py
@@ -0,0 +1,3 @@
+l = [1,2,3,4,5,6,7,1,2,3]
+
+print([i for i, x in enumerate(l) if x == 1])
